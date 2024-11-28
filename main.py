fe0dae104de8227cb24029549de1b98613594568
Roma <bshs71734@gmail.com>
2024-11-28 22:41:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from transport.client import Client
+from transport.vehicle import Vehicle
 
+if __name__ == "__main__":
+    client1 = Client("Alice", 50)
+    client2 = Client("Bob", 30, is_vip=True)
+
+    vehicle = Vehicle(capacity=100) 
+
+    print(vehicle) 
+
+  
