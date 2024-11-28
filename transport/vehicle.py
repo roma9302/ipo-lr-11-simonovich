87530ee5f9342c1108dd84cc20ce8e09c2a6c45d
Roma <bshs71734@gmail.com>
2024-11-28 14:53:02 +0300

    Class / attribute create

--- transport/vehicle.py
+++ transport/vehicle.py
@@ -1,1 +1,9 @@
+# Добавить валидацию и методы класса
+class Vehicle():
+	def __init__(self,vehicle_id,capacity,current_load,clients_list):
+		self.vehicle_id = vehicle_id
+		self.capacity = capacity
+		self.current_load = current_load
+		self.clients_list = clients_list
+
 
