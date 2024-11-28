970c71a6c223a7794e8e3b315874d100eb1c7274
Roma <bshs71734@gmail.com>
2024-11-28 22:40:56 +0300

    Update van.py

--- transport/van.py
+++ transport/van.py
@@ -1,1 +1,6 @@
+from .vehicle import *
 
+class Van(Vehicle):
+    def init(self, is_refrigerated , capacity, current_load, clients_list):
+        super().init( capacity, current_load, clients_list)
+        self.is_refrigerated = True if is_refrigerated.lower() == "да" else False
