295e5729859fd920e7c0bac08f56f21d040ef8ca
Roma <bshs71734@gmail.com>
2024-11-28 22:40:29 +0300

    Update airplane.py

--- transport/airplane.py
+++ transport/airplane.py
@@ -1,1 +1,6 @@
+from .vehicle import *
 
+class Airplane(Vehicle):
+    def init(self, max_altitude, capacity, current_load, clients_list):
+        super().init( capacity, current_load, clients_list)
+        self.max_altitude = validate_number(max_altitude)
