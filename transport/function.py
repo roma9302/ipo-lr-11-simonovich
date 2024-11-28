795487fa78ea5070643e82be005d328e73ddff21
Roma <bshs71734@gmail.com>
2024-11-28 13:41:46 +0300

    Update function.py

--- transport/function.py
+++ transport/function.py
@@ -1,25 +1,24 @@
 #Логика нахождения минимально возможного количества грузовиков т/к
-# добавить такую темку ,чтобы оно еще и писало какой тип т/с аироплан или фургон
-def main(transports): 
-    gruz = 7
+def main(transports):
+    gruz = 42
     counter = 0
     list_ts = []
 
-    def get_ts(vehicle):  #функция для принятия грузоподъемности каждой из машин
+    def get_ts(vehicle):
         return vehicle['ts']
 
     
-    transports.sort(key=get_ts)  #сортировка (здесь они сортируются в обратном порядке [1,2,3] )
-    transports.reverse()  #тут мы переворачиваем сортированый список ([3,2,1])
+    transports.sort(key=get_ts)
+    transports.reverse()
 
-    for transport in transports: #логика нахождения груза
+    for transport in transports:
         if gruz >= transport['ts']:
             while gruz > 0 and transport['ts'] <= gruz:
                 gruz -= transport['ts']  
-                transport['count'] += 1  #добавление к количеству use грузовиков
-                counter += 1  # общий счетчик грузовиков
-
-    if gruz > 0:  #логика нахождения колва авто,если груз меньше ,чем грузоподъемность
+                transport['count'] += 1  
+                counter += 1  
+                
+    if gruz > 0:  
         is_have = None
         for transport in transports: 
             if transport['ts'] > gruz:
@@ -32,21 +31,20 @@ def main(transports):
     print("Количество использованного транспорта:", counter)
     for transport in transports:
         if transport['count'] > 0:
-            print(f"Транспорт с вместимостью {transport['ts']} использован {transport['count']} раз")
+            print(f"Транспорт с вместимостью {transport['ts']} использован {transport['count']} раз(а) тип транспорта {transport['type']}")
 
     print("Минимально подходящий транспорт для оставшегося груза:", list_ts)
 
-
-"""t1 = {'ts': 40, 'count': 0}
-t2 = {'ts': 2, 'count': 0}
-t3 = {'ts': 8, 'count': 0}
-t4 = {'ts': 10, 'count': 0}
-t5 = {'ts': 8, 'count': 0}
+"""
+t1 = {'ts': 40, 'count': 0 , 'type': 'airplane' }
+t2 = {'ts': 2, 'count': 0, 'type' :'van' }
+t3 = {'ts': 8, 'count': 0,'type': 'van' }
+t4 = {'ts': 10, 'count': 0 ,' type ' :'airplane'}
+t5 = {'ts': 8, 'count': 0 ,' type' : 'van '}
 tra = [t1, t2, t3, t4, t5]
 
 main(tra)
 """
 
 
-
-#чото другое
+# чото другое
