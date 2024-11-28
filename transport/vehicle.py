8e7a311d31c52cc3e2440ef70707543e65b5cbf5
Roma <bshs71734@gmail.com>
2024-11-28 22:42:00 +0300

    Update vehicle.py

--- transport/vehicle.py
+++ transport/vehicle.py
@@ -1,9 +1,17 @@
-# Добавить валидацию и методы класса
-class Vehicle():
-	def __init__(self,vehicle_id,capacity,current_load,clients_list):
-		self.vehicle_id = vehicle_id
-		self.capacity = capacity
-		self.current_load = current_load
-		self.clients_list = clients_list
+from .function import generate, validate_number
+from .client import Client
 
+class Vehicle:
+    def __init__(self, capacity, current_load=0, clients_list=None):
+        self.vehicle_id = generate()  
+        self.capacity = validate_number(capacity)  
+        self.current_load = validate_number(current_load)  
+        self.clients_list = clients_list 
+    def __str__(self):
+        return f"ID ТС: {self.vehicle_id}, Грузоподъемность: {self.capacity} т, Текущая загрузка: {self.current_load} т"
 
+if __name__ == "__main__":
+    client1 = Client("Alice", 5)
+    client2 = Client("Bob", 3, is_vip=True)
+    vehicle = Vehicle(capacity=10)
+    print(vehicle)  
