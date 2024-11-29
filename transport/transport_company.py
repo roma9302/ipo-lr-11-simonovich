a7611245c831b0cc6454db6d3726d28a8204cff7
Roma <bshs71734@gmail.com>
2024-11-29 10:43:45 +0300

    Update transport_company.py

--- transport/transport_company.py
+++ transport/transport_company.py
@@ -1,1 +1,26 @@
+from .client import Client
+from .vehicle import Vehicle
+from .function import *
+
+class TransportCompany:
+    def __init__(self, name):
+        self.name = name
+        self.vehicles = []
+        self.clients = []
+
+    def add_vehicle(self, vehicle):
+        self.vehicles.append(vehicle)
+
+    def add_client(self, client):
+        self.clients.append(client)
+
+    def output_vehicle(self, client):
+        def __str__(self):
+            for i in range(len(self.vehicles)):
+                return f"{vehicles[i]}"
+
+
+
+
+
 
