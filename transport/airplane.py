4f19ee2831f5010f9e424600e0a31b25d1afc706
Roma <bshs71734@gmail.com>
2024-12-01 20:50:35 +0300

    Update airplane.py

--- transport/airplane.py
+++ transport/airplane.py
@@ -6,6 +6,7 @@ class Airplane(Vehicle):
         super().__init__(capacity, current_load, clients_list)
         self.max_altitude = validate_number(max_altitude)
 
+        #Словарь для записи в файл обьеккта класса
         airplane_data = {
             "type": 'airplane',
             "vehicle_id": self.vehicle_id, 
@@ -17,8 +18,12 @@ class Airplane(Vehicle):
         try:
             with open("transport/database.json", 'r', encoding='utf-8') as file:
                 data = json.load(file)
+                
+                #Если в файле нет нужной структуры , создаем ее
                 if "fields" not in data or "vehicles" not in data["fields"]:
                     data = {"company": "transport_company", "fields": {"vehicles": []}}
+                    
+        #Если в файле ошибочная структура , обновляем ее           
         except Exception:
             data = {"company": "transport_company", "fields": {"vehicles": []}}
 
