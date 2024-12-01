e92350cd9cb0fae12433c1125df39dcfbce4e491
Roma <bshs71734@gmail.com>
2024-12-01 19:23:07 +0300

    Update client.py

--- transport/client.py
+++ transport/client.py
@@ -1,3 +1,4 @@
+import json
 from .function import *
 
 class Client: 
@@ -5,3 +6,33 @@ class Client:
         self.name = validate_str(name)  
         self.cargo_weight = validate_number(cargo_weight)
         self.is_vip = is_vip
+        self.client_id = generate()
+
+
+
+        client_data = {
+            "client": {  
+                "name": self.name,
+                "cargo_weight": self.cargo_weight,
+                "is_vip": self.is_vip,
+                "client_id" : self.client_id
+            }
+        }
+
+        try:
+            with open("transport/clients.json", 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                if "clients" not in data:
+                    data = {"clients": []}
+                    
+        except Exception:
+            data = {"clients": []}
+
+
+        data["clients"].append(client_data)
+        
+
+        with open('transport/clients.json', 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=2)
+
+
