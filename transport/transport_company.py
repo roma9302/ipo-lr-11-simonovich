d0bc4ecac199942426822a13a4964e6455e0026e
Roma <bshs71734@gmail.com>
2024-12-01 19:25:53 +0300

    Update transport_company.py

--- transport/transport_company.py
+++ transport/transport_company.py
@@ -1,6 +1,7 @@
 from .client import Client
-from .vehicle import Vehicle
+from .vehicle import *
 from .function import *
+import json
 
 class TransportCompany:
     def __init__(self, name):
@@ -14,13 +15,46 @@ class TransportCompany:
     def add_client(self, client):
         self.clients.append(client)
 
-    def output_vehicle(self, client):
-        def __str__(self):
-            for i in range(len(self.vehicles)):
-                return f"{vehicles[i]}"
 
+    def list_vehicles(self):
+        try:
+            with open("transport/database.json", 'r', encoding='utf-8') as file:
+                vehicles_data = json.load(file)
+                
+            try:
+                vehicles = vehicles_data["fields"]["vehicles"]
+            except:
+                print("Возникла ошибка при выводе т/с . Их нет")
 
+            print("Доступные Транспортные средства:")
+            for vehicle_data in vehicles:
+                vehicle_id = vehicle_data['vehicle_id']
+                vehicle_type = vehicle_data['type']
+                capacity = vehicle_data['capacity']
+                current_load = vehicle_data['current_load']
+                is_refrigerated = vehicle_data.get('is_refrigerated') 
+                max_altitude = vehicle_data.get('max_altitude')
 
+                if vehicle_type == 'van':
+                    print(f"""
+                            Тип: {vehicle_type}
+                            Айди: {vehicle_id}
+                            Грузоподьемность: {capacity}
+                            Текущая загруженность: {current_load}
+                            Наличие холодильника: {is_refrigerated}
+                            """)
+                elif vehicle_type == 'airplane':
+                    print(f"""
+                            Тип: {vehicle_type}
+                            Айди: {vehicle_id}
+                            Грузоподьемность: {capacity}
+                            Текущая загруженность: {current_load}
+                            Максимальная высота подьема {max_altitude}
+                            """)
+        except:
+            print("Ошибка при выводе транспорта. Проверьте его наличие")
 
+    def optimize_cargo_distribution(self):
+        optimize()
 
 
